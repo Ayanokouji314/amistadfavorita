62464e360ebd9142d6fe73be48c8d732ee038177
Ayanokouji314 <168379954+Ayanokouji314@users.noreply.github.com>
2024-04-28 21:08:40 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("sin").addEventListener("click", function() {
+
+    document.getElementById("sin").style.display = "none";
+    document.getElementById("peticion").style.display = "none";
+    document.getElementById("cion").style.display = "none";
+    document.getElementById("peti").style.display = "none";
+    document.getElementById("carta").style.visibility = "visible";
+    
+});
