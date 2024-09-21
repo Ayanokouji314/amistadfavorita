ed4f61123aaabb202c9ffec057f009ee60ffb255
Ayanokouji314 <168379954+Ayanokouji314@users.noreply.github.com>
2024-09-21 16:44:00 -0400

    main.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-document.getElementById("sin").addEventListener("click", function() {
-
-    document.getElementById("sin").style.display = "none";
-    document.getElementById("peticion").style.display = "none";
-    document.getElementById("cion").style.display = "none";
-    document.getElementById("peti").style.display = "none";
-    document.getElementById("carta").style.visibility = "visible";
-    
-});
+onload = () => {
+  document.body.classList.remove("container");
+};
